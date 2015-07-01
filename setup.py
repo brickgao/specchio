25c9fbf148cb7ff2005a14d1f1d405ee5cef542a
brickgao <brickgao@gmail.com>
2015-07-01 13:31:41 +0800

    Fix setup.py, add tests_require and test_suite

--- setup.py
+++ setup.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from setuptools import find_packages, setup
 from specchio import __version__
+
+from setuptools import find_packages, setup
 
 setup(
     name="Specchio",
@@ -12,19 +13,31 @@ setup(
     description="Specchio is a tool that can help you rsync your file,"
                 "it use `.gitignore` in git to discern which file is ignored.",
     long_description=open("README.md").read(),
-    author="Brickgao",
+    author="Xiongzhi Gao",
     author_email="brickgao@gmail.com",
     packages=find_packages(exclude=["tests"]),
     zip_safe=False,
     install_requires=[
-        "colorlog",
-        "mock",
-        "testfixtures",
-        "watchdog"
+        "colorlog >= 2.6.0 < 3.0.0",
+        "watchdog >= 0.8.3 < 1.0.0"
     ],
+    tests_require=[
+        "mock >= 1.0.1 < 2.0.0",
+        "testfixtures >= 4.1.2 < 5.0.0"
+    ],
+    test_suite="nose.collector",
     entry_points={
         "console_scripts": [
             "specchio = specchio.main:main"
         ],
     },
+    classifiers=[
+        "Environment :: Console",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2",
+        "Topic :: Software Development :: Testing",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+    ],
 )
