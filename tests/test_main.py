5fd15ebc29e8bbdabefe86cdddbc1c0ba5102d30
brickgao <brickgao@gmail.com>
2015-07-06 18:01:27 +0800

    Test case follow up the change
    Test for the case that there is no `ssh` or `rsync` in the system

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,28 +1,77 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import io
 from unittest import TestCase
 
 import mock
+from testfixtures import LogCapture
 
 from specchio.main import main
 
 
 class mainTest(TestCase):
 
+    @mock.patch("specchio.main.os")
+    @mock.patch("specchio.main.sys")
+    @mock.patch("specchio.main.SpecchioEventHandler")
+    def test_main_invalid(self, _SpecchioEventHandler, _sys, _os):
+        _sys.argv = ["specchio"]
+        _arg2ret = {
+            "ssh -V": io.StringIO(u"test_msg"),
+            "rsync --version": io.StringIO(u"test_msg")
+        }
+        _os.popen.side_effect = (lambda arg: _arg2ret[arg])
+        main()
+        self.assertEqual(_SpecchioEventHandler.call_count, 0)
+
+    @mock.patch("specchio.main.os")
     @mock.patch("specchio.main.sys")
     @mock.patch("specchio.main.init_logger")
-    def test_main_invalid(self, _init_logger, _sys):
+    def test_main_no_ssh(self, _init_logger, _sys, _os):
+        _init_logger.return_value = True
         _sys.argv = ["specchio"]
-        main()
-        self.assertEqual(_init_logger.call_count, 0)
+        _arg2ret = {
+            "ssh -V": io.StringIO(u"")
+        }
+        _os.popen.side_effect = (lambda arg: _arg2ret[arg])
+        with LogCapture() as log_capture:
+            main()
+            log_capture.check(
+                ("specchio", "ERROR", "Specchio need `ssh`, "
+                                      "but there is no `ssh` in the system")
+            )
 
+    @mock.patch("specchio.main.os")
+    @mock.patch("specchio.main.sys")
+    @mock.patch("specchio.main.init_logger")
+    def test_main_no_rsync(self, _init_logger, _sys, _os):
+        _init_logger.return_value = True
+        _sys.argv = ["specchio"]
+        _arg2ret = {
+            "ssh -V": io.StringIO(u"test_msg"),
+            "rsync --version": io.StringIO(u"")
+        }
+        _os.popen.side_effect = (lambda arg: _arg2ret[arg])
+        with LogCapture() as log_capture:
+            main()
+            log_capture.check(
+                ("specchio", "ERROR", "Specchio need `rsync`, "
+                                      "but there is no `rsync` in the system")
+            )
+
+    @mock.patch("specchio.main.os")
     @mock.patch("specchio.main.sys")
     @mock.patch("specchio.main.time")
     @mock.patch("specchio.main.Observer")
     @mock.patch("specchio.main.SpecchioEventHandler")
     def test_main(self, _SpecchioEventHandler,
-                  _Observer, _time, _sys):
+                  _Observer, _time, _sys, _os):
+        _arg2ret = {
+            "ssh -V": io.StringIO(u"test_msg"),
+            "rsync --version": io.StringIO(u"test_msg")
+        }
+        _os.popen.side_effect = (lambda arg: _arg2ret[arg])
         _sys.argv = ["specchio", "/a/", "user@host:/b/a/"]
         _event_handler = mock.Mock()
         _SpecchioEventHandler.return_value = _event_handler
