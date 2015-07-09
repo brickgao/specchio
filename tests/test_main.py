560fa9046671a9f8a6b95d7a6c0b3f98df487e12
brickgao <brickgao@gmail.com>
2015-07-09 14:31:18 +0800

    Add test case for specchio.main() with wrong options

--- tests/test_main.py
+++ tests/test_main.py
@@ -12,6 +12,10 @@ from specchio.main import main
 
 class mainTest(TestCase):
 
+    _GENERAL_OPTIONS = GENERAL_OPTIONS = {
+        "--init-remote"
+    }
+
     @mock.patch("specchio.main.os")
     @mock.patch("specchio.main.sys")
     @mock.patch("specchio.main.SpecchioEventHandler")
@@ -87,10 +91,28 @@ class mainTest(TestCase):
                 ("specchio", "INFO", "Specchio stopped, have a nice day :)")
             )
         _SpecchioEventHandler.assert_called_once_with(
-            src_path="/a/", dst_ssh="user@host", dst_path="/b/a/"
+            src_path="/a/", dst_ssh="user@host", dst_path="/b/a/",
+            is_init_remote=False
         )
         _observer_object.schedule.assert_called_once_with(
             _event_handler, "/a/", recursive=True
         )
         _observer_object.stop.assert_called_once_with()
         _observer_object.join.assert_called_once_with()
+
+    @mock.patch("specchio.main.os")
+    @mock.patch("specchio.main.sys")
+    @mock.patch("specchio.main.init_logger")
+    @mock.patch("specchio.main.SpecchioEventHandler")
+    @mock.patch("specchio.main.GENERAL_OPTIONS", _GENERAL_OPTIONS)
+    def test_main_with_wrong_options(self, _SpecchioEventHandler,
+                                     _init_logger, _sys, _os):
+        _arg2ret = {
+            "whereis ssh": io.StringIO(u"test_msg"),
+            "whereis rsync": io.StringIO(u"test_msg")
+        }
+        _init_logger.return_value = True
+        _os.popen.side_effect = (lambda arg: _arg2ret[arg])
+        _sys.argv = ["specchio", "--test", "/a/", "user@host:/b/a/"]
+        main()
+        self.assertEqual(_SpecchioEventHandler.call_count, 0)
