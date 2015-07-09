3853b525e472742771f94cffc6bc5581adb056dc
brickgao <brickgao@gmail.com>
2015-07-09 15:09:27 +0800

    Add test case for SpecchioEventHandler.init_remote

--- tests/test_handlers.py
+++ tests/test_handlers.py
@@ -35,7 +35,7 @@ class SpecchioEventHandlerTest(TestCase):
         }
         self.handler.gitignore_list = ["/a/"]
 
-    def is_ignore_git_folder_test(self):
+    def test_is_ignore_git_folder(self):
         _file_or_dir_path = mock.Mock("/a/")
         _file_or_dir_path.startswith.return_value = True
         result = self.handler.is_ignore(_file_or_dir_path)
@@ -44,7 +44,7 @@ class SpecchioEventHandlerTest(TestCase):
 
     @mock.patch("specchio.handlers.dfs_get_gitignore")
     @mock.patch("specchio.handlers.get_all_re")
-    def init_gitignore_test(self, _get_all_re, _dfs_get_gitignore):
+    def test_init_gitignore(self, _get_all_re, _dfs_get_gitignore):
         _dfs_get_gitignore.return_value = ["/a/.gitignore"]
         _get_all_re.return_value = {
             "/a/.gitignore": {
@@ -70,7 +70,7 @@ class SpecchioEventHandlerTest(TestCase):
 
     @mock.patch("specchio.handlers.os")
     @mock.patch("specchio.handlers.remote_create_folder")
-    def on_created_folder_test(self, _remote_create_folder, _os):
+    def test_on_created_folder(self, _remote_create_folder, _os):
         _remote_create_folder.return_value = True
         _os.path.abspath.return_value = "/a/test1.py"
         _os.path.join.return_value = "/b/a/test1.py"
@@ -84,7 +84,7 @@ class SpecchioEventHandlerTest(TestCase):
     @mock.patch("specchio.handlers.remote_create_folder")
     @mock.patch("specchio.handlers.rsync")
     @mock.patch("specchio.handlers.os")
-    def on_created_file_test(self, _os, _rsync,
+    def test_on_created_file(self, _os, _rsync,
                              _remote_create_folder):
         with mock.patch.object(
                 self.handler,
@@ -108,7 +108,7 @@ class SpecchioEventHandlerTest(TestCase):
 
     @mock.patch("specchio.handlers.os")
     @mock.patch("specchio.handlers.remote_create_folder")
-    def on_created_ignore_test(self, _remote_create_folder, _os):
+    def test_on_created_ignore(self, _remote_create_folder, _os):
         _remote_create_folder.return_value = True
         _os.path.abspath.return_value = "/a/test.py"
         _event = DirCreatedEvent(src_path="/a/test.py")
@@ -118,7 +118,7 @@ class SpecchioEventHandlerTest(TestCase):
     @mock.patch("specchio.handlers.os")
     @mock.patch("specchio.handlers.remote_create_folder")
     @mock.patch("specchio.handlers.rsync")
-    def on_modified_test(self, _rsync, _remote_create_folder, _os):
+    def test_on_modified(self, _rsync, _remote_create_folder, _os):
         with mock.patch.object(
                 self.handler,
                 "update_gitignore"
@@ -146,7 +146,7 @@ class SpecchioEventHandlerTest(TestCase):
     @mock.patch("specchio.handlers.os")
     @mock.patch("specchio.handlers.remote_create_folder")
     @mock.patch("specchio.handlers.rsync")
-    def on_modifited_ignore_test(self, _rsync, _remote_create_folder, _os):
+    def test_on_modifited_ignore(self, _rsync, _remote_create_folder, _os):
         _rsync.return_value = True
         _remote_create_folder.return_value = True
         _os.path.abspath.return_value = "/a/test.py"
@@ -156,7 +156,7 @@ class SpecchioEventHandlerTest(TestCase):
         self.assertEqual(_rsync.call_count, 0)
 
     @mock.patch("specchio.handlers.get_all_re")
-    def update_gitignore_test(self, _get_all_re):
+    def test_update_gitignore(self, _get_all_re):
         _get_all_re.return_value = {
             "/a/b/.gitignore": {
                 1: [],
@@ -189,7 +189,7 @@ class SpecchioEventHandlerTest(TestCase):
         self.handler.gitignore_list = _handler_gitignore_list
         self.handler.gitignore_dict = _handler_gitignore_dict
 
-    def del_gitignore_test(self):
+    def test_del_gitignore(self):
         _handler_gitignore_list = list(self.handler.gitignore_list)
         _handler_gitignore_dict = dict(self.handler.gitignore_dict)
         self.handler.del_gitignore("/a/.gitignore")
@@ -206,7 +206,7 @@ class SpecchioEventHandlerTest(TestCase):
 
     @mock.patch("specchio.handlers.os")
     @mock.patch("specchio.handlers.remote_rm")
-    def on_deleted_test(self, _remote_rm, _os):
+    def test_on_deleted(self, _remote_rm, _os):
         with mock.patch.object(
             self.handler,
             "del_gitignore"
@@ -229,8 +229,7 @@ class SpecchioEventHandlerTest(TestCase):
             _del_gitignore.assert_called_once_with("/a/.gitignore")
 
     @mock.patch("specchio.handlers.os")
-    @mock.patch("specchio.handlers.remote_rm")
-    def on_delete_ignore_test(self, _remote_rm, _os):
+    def test_on_delete_ignore(self, _os):
         _os.path.abspath.return_value = "/a/test.py"
         _event = FileDeletedEvent(src_path="/a/test.py")
         self.handler.on_deleted(_event)
@@ -241,7 +240,7 @@ class SpecchioEventHandlerTest(TestCase):
 
     @mock.patch("specchio.handlers.os")
     @mock.patch("specchio.handlers.remote_mv")
-    def on_moved_test(self, _mv, _os):
+    def test_on_moved(self, _mv, _os):
         _mv.return_value = True
         _os.path.abspath.side_effect = ["/a/1.py", "/a/2.py"]
         _os.path.join.side_effect = ["/b/a/1.py", "/b/a/2.py"]
@@ -258,7 +257,7 @@ class SpecchioEventHandlerTest(TestCase):
     @mock.patch("specchio.handlers.remote_create_folder")
     @mock.patch("specchio.handlers.remote_mv")
     @mock.patch("specchio.handlers.rsync")
-    def on_moved_all_ignore_test(self, _rsync, _mv, _create_folder, _rm, _os):
+    def test_on_moved_all_ignore(self, _rsync, _mv, _create_folder, _rm, _os):
         _mv.return_value = True
         _create_folder.return_value = True
         _rm.return_value = True
@@ -275,7 +274,7 @@ class SpecchioEventHandlerTest(TestCase):
     @mock.patch("specchio.handlers.os")
     @mock.patch("specchio.handlers.remote_create_folder")
     @mock.patch("specchio.handlers.rsync")
-    def on_moved_src_ignore_test(self, _rsync, _create_folder, _os):
+    def test_on_moved_src_ignore(self, _rsync, _create_folder, _os):
         _create_folder.return_value = True
         _rsync.return_value = True
         _os.path.abspath.side_effect = ["/a/test.py", "/a/1.py"]
@@ -294,7 +293,7 @@ class SpecchioEventHandlerTest(TestCase):
 
     @mock.patch("specchio.handlers.os")
     @mock.patch("specchio.handlers.remote_rm")
-    def on_moved_dst_ignore_test(self, _rm, _os):
+    def test_on_moved_dst_ignore(self, _rm, _os):
         _rm.return_value = True
         _os.path.abspath.side_effect = ["/a/1.py", "/a/test.py"]
         _os.path.join.side_effect = ["/b/a/1.py", "/b/a/test.py"]
@@ -303,4 +302,30 @@ class SpecchioEventHandlerTest(TestCase):
         _rm.assert_called_once_with(
             dst_ssh=self.handler.dst_ssh,
             dst_path="/b/a/1.py"
+        )
+
+    @mock.patch("specchio.handlers.os")
+    @mock.patch("specchio.handlers.rsync")
+    @mock.patch("specchio.handlers.remote_create_folder")
+    def test_init_remote(self, _create_folder, _rsync, _os):
+        _os.walk.return_value = [["/a/", [], ["1.py", "2.py"]]]
+        _os.path.abspath.side_effect = [
+            "/a/1.py", "/a/2.py"
+        ]
+        _os.path.join.side_effect = [
+            "/b/a",
+            "/a/1.py", "/b/a/1.py",
+            "/a/2.py", "/b/a/2.py"
+        ]
+        _rsync.return_value = True
+        _create_folder.return_value = True
+        with mock.patch.object(self.handler, "is_ignore") as _is_ignore:
+            _is_ignore.side_effect = [True, False]
+            self.handler.init_remote()
+        _create_folder.assert_called_once_with(
+            dst_ssh=self.handler.dst_ssh, dst_path="/b/a"
+        )
+        _rsync.assert_called_once_with(
+            dst_ssh=self.handler.dst_ssh, src_path="/a/2.py",
+            dst_path="/b/a/2.py",
         )
